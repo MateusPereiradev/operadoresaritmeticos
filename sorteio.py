2660084deb0c98ed2471e37c8285212271aa3569
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-10 20:36:14 -0300

    Sorteio

--- sorteio.py
+++ sorteio.py
@@ -1,6 +1,6 @@
 from random import choice
-alunos= str(input("Digite aqui o nome dos alunos separados por vírgula:")).strip().split(",")
-print(f"Referente aos alunos, o escolhido foi {choice(alunos)}")
+alunos= str(input('Digite aqui o nome dos alunos participantes separados por vírgula:')).strip().upper().split(',')
+print(f'Referente aos alunos, quem ganhou o sorteio para apagar o quadro? {choice(alunos)}')
 
 '''
 Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
