fbf46c4d6ff495042710ec46f8952c6cf7f93627
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-10 20:31:49 -0300

    Seno, cosseno e tangente

--- seno.py
+++ seno.py
@@ -1,10 +1,9 @@
 from math import sin, cos, tan, radians
-angulo= float(input("Digite aqui um ângulo:"))
-print(f"Referente ao angulo {angulo} o valor de seno é {sin(radians(angulo)):.2f}")
-print(f"Referente ao angulo {angulo} o valor de cosseno é {cos(radians(angulo)):.2f}")
-print(f"Referente ao angulo {angulo} o valor da tangente é {tan(radians(angulo)):.2f}")
-
-
+angulo= float(input('Digite aqui um ângulo:'))
+print(f'Referente ao ângulo {angulo} o seno é {sin(radians(angulo)):.2f}')
+print(f'Referente ao ângulo {angulo} o cosseno é {cos(radians(angulo)):.2f}')
+print(f'Referente ao ângulo {angulo} a tangente é {tan(radians(angulo)):.2f}')
+print('Fim do programa!')
 
 '''
 Faça um programa que leia um ângulo qualquer e mostre na tela o valor
